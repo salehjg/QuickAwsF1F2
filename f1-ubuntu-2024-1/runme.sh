5f2790b4a719d792ab46d115cbb6b54bc115b3b3
saleh <salehjamaligolzar@gmail.com>
2025-03-17 13:33:27 +0100

    Added full.log to runme.sh

--- f1-ubuntu-2024-1/runme.sh
+++ f1-ubuntu-2024-1/runme.sh
@@ -1,9 +1,10 @@
 # import common.sh
 source common.sh
 
-
-recipe_build_cmake
-recipe_vnc_server
-recipe_clone_aws_repo
-recipe_setup_aws_vitis
-recipe_setup_aws_xrt
+{
+    recipe_build_cmake
+    recipe_vnc_server
+    recipe_clone_aws_repo
+    recipe_setup_aws_vitis
+    recipe_setup_aws_xrt
+} &> full.log
