6f8a903230825c9ab7cd2762cfe902302f10a05f
saleh <salehjamaligolzar@gmail.com>
2025-03-20 09:34:48 +0100

    Some comments regarding the tcl init issue.

--- f1-ubuntu-2024-1/runme.sh
+++ f1-ubuntu-2024-1/runme.sh
@@ -1,6 +1,10 @@
 # import common.sh
 source common.sh
 
+# https://adaptivesupport.amd.com/s/article/51582?language=en_US
+# Users should not add xilinx to their LD_LIBRARY_PATH, but instead rely on the loaders
+# (Vivado and ISE DS applications have boot loaders) to set up the necessary environment needed to run the application.
+
 {
     recipe_install_utils
     recipe_install_cmake_from_apt
@@ -10,3 +14,4 @@ source common.sh
     recipe_setup_aws_vitis
     recipe_setup_aws_xrt
 } 2>&1 | tee full.log
+
